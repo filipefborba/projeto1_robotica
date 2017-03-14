e8b424dd978b02fe7424c350eacfb2ec1fb64eb0
Martim <martim.ferreira.jose@gmail.com>
2017-03-13 21:11:25 -0300

    modficaçoes nas maquinas de estado

--- scripts/maquina_estados_survival.py
+++ scripts/maquina_estados_survival.py
@@ -1,7 +1,8 @@
-### Survival (1) - Bumper
+from math import radians
+
 class Moving(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['Not_yet','Crash'])
+        smach.State.__init__(self, outcomes=['Not_yet','Crash', 'Near'])
 
     def execute(self, userdata):
         global speed_output
@@ -10,41 +11,44 @@ class Moving(smach.State):
         speed_output.publish(speed)
 
 #Survival Instincts
-        if bumper bater em algo OR sensor receber que robo está proximo de obstáculo:
+        if sensor receber que robo está proximo de obstáculo:
             speed = Twist(Vector3(0, 0, 0), Vectetor3(0, 0, 0)) #Parar
-            rospy.sleep(0.05) #Tempo de espera
-            speed = Twist(Vector3(-1, 0, 0), Vectetor3(0, 0, 0))
+            return  'Near'
+        if bumper bater em algo:
+            speed = Twist(Vector3(0, 0, 0), Vectetor3(0, 0, 0)) #Parar
             return 'Crash'
         else:
             return 'Not_yet'
 
+class Turning(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['Turned'])
+
+    def execute(self, userdata):
+        global speed_output
+        speed = Twist(Vector3(0, 0, 0), Vector3(0, 0, (radians(90)))) #Girar 90 graus
+        speed_output.publish(speed)
+
+
+
 
 def main():
     global speed_output
-    speed = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    speed_output= rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     bumper = rospy.Publisher('/bump', Twist, queue_size=1)
     laser = rospy.Publisher('/scan', Twist, queue_size=1)
 
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['terminei'])
+    sm = smach.StateMachine(outcomes=['Finish'])
 
     # Open the container
     with sm:
-        # Add states to the container
-        smach.StateMachine.add('LONGE', Longe(),
-                               transitions={'ainda_longe':'ANDANDO',
-                                            'perto':'GIRANDO'})
-        smach.StateMachine.add('ANDANDO', Andando(),
-                               transitions={'ainda_longe':'LONGE'})
-        smach.StateMachine.add('GIRANDO', Girando(),
-                                transitions={'alinhando': 'GIRANDO',
-                                'alinhou':'LONGE2'})
-        smach.StateMachine.add('LONGE2', Longe(),
-                               transitions={'ainda_longe':'ANDANDO2',
-                                            'perto':'terminei'})
-        smach.StateMachine.add('ANDANDO2', Andando(),
-                               transitions={'ainda_longe':'LONGE2'})
+        #Adicionando máquina de estados
+        smach.StateMachine.add('LONGE', Longe(), transitions={'ainda_longe':'ANDANDO', 'perto':'GIRANDO'})
+        #LONGE, se retornar ainda_longe, executa ANDANDO. Se retornar perto, executa GIRANDO
+
+
 
 
     # Execute SMACH plan
